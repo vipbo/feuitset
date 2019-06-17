52e81848fd2f2a847d7f99440049aacd35555668
bo <liu@qq.com>
2019-06-17 14:01:40 +0800

    webpack 3.6.0 and add less less-loader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,9 +26,13 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
+<style scoped lang='less'>
+.hello {
+  color: yellow;
+  h1,
+  h2 {
+    font-weight: normal;
+    color: aqua;
+  }
 }
 </style>
