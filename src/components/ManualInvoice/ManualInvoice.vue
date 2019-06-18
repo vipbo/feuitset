0b2c50fd9366ae6f53a1f5b617a7340a32b14148
bo <liu@qq.com>
2019-06-18 11:22:43 +0800

    edit static manualPage

--- src/components/ManualInvoice/ManualInvoice.vue
+++ src/components/ManualInvoice/ManualInvoice.vue
@@ -4,8 +4,14 @@
             <div slot="title">手工开票</div>
         </m-header>
         <div class="company">恭喜发财有限公司</div>
+        <div class='p-component'>
+           <p-component >发票信息</p-component>
+        </div>
+        <fe-tab>
+            <fe-tabitem selected >企业</fe-tabitem>
+            <fe-tabitem >个人/其他</fe-tabitem>
+        </fe-tab> 
         <div class="content">
-             <p-component>发票信息</p-component>
              <div class="type">
                  <div class="name">发票种类</div>
                  <div class="detail">
@@ -13,15 +19,8 @@
                      <i class="iconfont iconyoujiantou" ></i>
                  </div>
              </div>
-             <div class="up-head">
-                 <div>抬头类型</div>
-                 <div class="who">
-                    <div class="enterprise">企业</div>
-                    <div class="person">个人/其他</div>
-                 </div>
-             </div>
              <div class="oli">
-                 <div>成品油发票</div>
+                 <div>开成品油发票</div>
                 <fe-switch title=""></fe-switch>
              </div>
              <div class="invoice-item">
@@ -30,24 +29,82 @@
              </div>
              <div class="tax-item">
                  <div class="tax">企业税号</div>
-                 <input type="text" placeholder="必填，请输入15-20位企业税号">
+                 <input type="text" placeholder="必填，请输入6-20位企业税号">
              </div>
              <div :class="`extend ${ isExtend ?'low-height':'h-height'}`">
-                 <span @click="changeExtend">展开选填项</span>
-                 <i class="iconfont iconxiajiantou" ></i>
+                  <div class="target" @click="changeExtend" v-show='isExtend'>
+                       <span >展开选填项</span>
+                       <i class="iconfont iconxiajiantou" ></i>
+                  </div>               
+                  <div class="address">
+                      <div class="phone">地址电话</div>
+                      <input type="text" placeholder="请填写地址及其电话信息">
+                  </div>
+                  <div class="bank-info">
+                      <div class="bank-id">开户行及账号</div>
+                      <input type="text" placeholder="请输入开户行及账号">
+                  </div>
+                    <div class="note-info">
+                      <div class="note">备注</div>
+                      <input type="text" placeholder="请输入备注文案">
+                  </div>
+                  <div class="show-note">
+                      <div>发票显示备注</div>
+                      <fe-switch title=""></fe-switch>
+                  </div>
+                  <div class="pull-item"  @click="changeExtend">
+                      <span>收起</span>
+                       <i class="iconfont iconshangjiantou" ></i>
+                  </div>
              </div>
+        </div>
+        <div class="pay-type">
+            <div class='p-component'>
+                <p-component >支付方式</p-component>
+            </div>
+            <div class="receive-phone">
+                <div class="telephone">手机号码</div>
+                <input type="text" placeholder="请输入手机号码接收电子发票">
+            </div>
+            <div class="e-mail">
+                <div class="mail">邮箱地址</div>
+                <input type="text" placeholder="请输入邮箱地址接收电子发票">
+            </div>
+        </div>
+        <div class="invoice-detail-wrap">
+            <div class='p-component'>
+                <p-component >发票明细</p-component>
+            </div>
+            <invoice-detail />
+            <fe-button type="default" class='add-btn'>添加发票明细</fe-button>
+        </div>
+        <div class="foot-box">
+          <div  class="foot-area">
+            <div>
+              <span>总额(元):</span>
+              <span class="number">10984.00</span>
+            </div>
+            <div>
+              <fe-button type="primary" class='submit-btn'>提交开票</fe-button>
+            </div>
+          </div>
         </div>
     </div>
 </template>
 <script>
-import { Switch } from "feui";
+import { Tab, TabItem, Switch, Button } from "feui";
 import MHeader from "../common/MHeader";
 import PComponent from "../common/PComponent";
+import InvoiceDetail from "../common/InvoiceDetail";
 export default {
   components: {
+    [Tab.name]: Tab,
+    [TabItem.name]: TabItem,
     [Switch.name]: Switch,
+    [Button.name]: Button,
     MHeader,
-    PComponent
+    PComponent,
+    InvoiceDetail
   },
   data() {
     return {
@@ -70,14 +127,21 @@ export default {
     background: #007aff;
     text-align: center;
   }
-  .content {
+  .p-component {
+    line-height: 30px;
     padding: 10px;
+    background: #fff;
+    border-bottom: 1px solid #ccc;
+  }
+  .content {
     background: #fff;
     .type {
       display: flex;
-      margin: 10px auto;
+      // margin: 10px auto;
+      padding: 10px;
+      border-bottom: 1px solid #ccc;
       .name {
-        flex: 1;
+        flex: 1.8;
       }
       .detail {
         flex-grow: 1;
@@ -90,41 +154,21 @@ export default {
         }
       }
     }
-    .up-head {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin: 10px auto;
-      .who {
-        display: flex;
-        align-items: center;
-        .enterprise {
-          border: 1px solid #ccc;
-          padding: 2px 25px;
-          border-top-left-radius: 15px;
-          border-bottom-left-radius: 15px;
-        }
-        .person {
-          border: 1px solid #ccc;
-          padding: 2px 10px;
-          border-top-right-radius: 15px;
-          border-bottom-right-radius: 15px;
-        }
-      }
-    }
+
     .oli {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      margin: 10px auto;
+      padding: 10px;
       border-bottom: 1px solid #ccc;
     }
     .invoice-item {
       display: flex;
       align-items: center;
-      margin: 10px auto;
+      padding: 10px;
+      border-bottom: 1px solid #ccc;
       .invoice {
-        flex: 1;
+        flex: 1.5;
       }
       input {
         height: 25px;
@@ -135,11 +179,11 @@ export default {
     .tax-item {
       display: flex;
       align-items: center;
-      margin: 10px auto;
-      padding-bottom: 10px;
+
+      padding: 10px;
       border-bottom: 1px solid #ccc;
       .tax {
-        flex: 1;
+        flex: 1.5;
       }
       input {
         height: 25px;
@@ -148,16 +192,152 @@ export default {
       }
     }
     .extend {
-      text-align: center;
-      color: rgb(132, 197, 252);
+      overflow: hidden;
+      .target {
+        text-align: center;
+        padding: 10px;
+        color: #1990fa;
+      }
+      .address {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+        .phone {
+          flex: 1.5;
+        }
+        input {
+          height: 25px;
+          border: none;
+          flex-grow: 1;
+        }
+      }
+      .bank-info {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+        .bank-id {
+          flex: 1.5;
+        }
+        input {
+          height: 25px;
+          border: none;
+          flex-grow: 1;
+        }
+      }
+      .note-info {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+        .note {
+          flex: 1.5;
+        }
+        input {
+          height: 25px;
+          border: none;
+          flex-grow: 1;
+        }
+      }
+      .show-note {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+      }
+      .pull-item {
+        text-align: center;
+        padding: 10px;
+        color: #1990fa;
+      }
     }
     .low-height {
-      //   border: 1px solid #ccc;
-      height: 30px;
+      height: 50px;
     }
     .h-height {
-      //   border: 1px solid #ccc;
-      height: 300px;
+      height: 250px;
+    }
+  }
+  .pay-type {
+    background: #fff;
+    margin-top: 10px;
+    .p-component {
+      line-height: 30px;
+      padding: 10px;
+      background: #fff;
+      border-bottom: 1px solid #ccc;
+    }
+    .receive-phone {
+      display: flex;
+      align-items: center;
+
+      padding: 10px;
+      border-bottom: 1px solid #ccc;
+      .telephone {
+        flex: 1.5;
+      }
+      input {
+        height: 25px;
+        border: none;
+        flex-grow: 1;
+      }
+    }
+    .e-mail {
+      display: flex;
+      align-items: center;
+
+      padding: 10px;
+      border-bottom: 1px solid #ccc;
+      .mail {
+        flex: 1.5;
+      }
+      input {
+        height: 25px;
+        border: none;
+        flex-grow: 1;
+      }
+    }
+  }
+  .invoice-detail-wrap {
+    background: #fff;
+    padding: 10px;
+    margin-bottom: 70px;
+    .p-component {
+      line-height: 30px;
+      padding: 10px;
+      background: #fff;
+      border-bottom: 1px solid #ccc;
+    }
+    .add-btn {
+      width: 90%;
+      border: 1px solid #1990fa;
+      color: #1990fa;
+      background: #fff;
+      margin: 10px auto;
+    }
+  }
+  .foot-box {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: #fff;
+    width: 100%;
+    .foot-area {
+      display: flex;
+      height: 60px;
+      line-height: 60px;
+      padding: 0 15px;
+      background: #fff;
+      align-items: center;
+      justify-content: space-between;
+      .number {
+        color: #1990fa;
+      }
+      .submit-btn {
+        font-size: 16px;
+      }
     }
   }
 }
---
+++ src/components/common/InvoiceDetail.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="invoice-detail">
+        <div class="name">
+            <div class="shop">汽油</div>
+            <div class="oil-info">
+                <span class="tax">税率:16%</span>
+                <span>#92号汽油</span>
+            </div>
+        </div>
+        <div class="price">
+            <div>￥103.32</div>
+            <div class="delete">
+                <i class="iconfont iconshanchu" />
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+.invoice-detail {
+  display: flex;
+  padding: 0 10px 10px;
+  border-bottom: 1px solid #ccc;
+  .name {
+    flex: 1;
+    .shop {
+      font-weight: 600;
+    }
+    .oil-info {
+      font-size: 13px;
+      padding-top: 5px;
+      .tax {
+        background: darkorange;
+        color: #fff;
+        padding: 0 5px;
+        border-radius: 3px;
+      }
+    }
+  }
+  .price {
+    .delete {
+      text-align: right;
+      i {
+        font-size: 13px;
+      }
+    }
+  }
+}
+</style>
