76bf5920e2fad849a12389621f34ace7ff1cc22e
bo <liu@qq.com>
2019-07-05 15:57:03 +0800

    add test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,57 @@
 <template>
   <div class="home">
-   <!-- <fe-button type="primary" >页面主要操作</fe-button>
+    <!-- <fe-button type="primary" >页面主要操作</fe-button>
    <fe-button type="warn" mini>页面主要操作</fe-button>
    <fe-button type="default" disabled>disabled</fe-button>
    <fe-qrcode value="https://feui.gitee.io"></fe-qrcode> -->
-    <m-header >
+    <m-header>
       <div slot="title">XXXX</div>
       <!-- <div slot="add">添加</div> -->
     </m-header>
     <div class="top"></div>
-      <div class="company" >
-        <img src="../assets/img/zwt.png" alt="" >
-        <div class='text'>
-          <div class="name" @click='showDetailName'>
-              <span class="detail">恭喜发财有限公司</span>
-             <i :class="`iconfont iconfanhui ${isRotate?'is-roate270':'is-roate90'}`" ></i>
-          </div> 
-          <div class="other">开票代码：4615</div>
+    <div class="company">
+      <img src="../assets/img/zwt.png" alt="" />
+      <div class="text">
+        <div class="name" @click="showDetailName">
+          <span class="detail">恭喜发财有限公司</span>
+          <i
+            :class="
+              `iconfont iconfanhui ${isRotate ? 'is-roate270' : 'is-roate90'}`
+            "
+          ></i>
         </div>
+        <div class="other">开票代码：4615</div>
       </div>
-    <div class="content"  >
-      <router-link to='manualInvoice' class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>手工开票</div>
+    </div>
+    <div class="content">
+      <router-link to="manualInvoice" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>手工开票</div>
       </router-link>
-      <router-link to='scanInvoice' class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>扫码开票</div>
+      <router-link to="scanInvoice" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>扫码开票</div>
       </router-link>
-      <router-link to='checkInvoice' class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>审核开票</div>
+      <router-link to="checkInvoice" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>审核开票</div>
       </router-link>
-      <router-link to='invoiceRecord' class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>开票记录</div>
-      </router-link >
-      <router-link to='goodsInfo' class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>商品信息</div>
-      </router-link >
-      <router-link to='invoiceInfo'  class="item">
-          <i class="iconfont iconshenfenzheng" ></i>
-          <div>开票信息</div>
-      </router-link >
+      <router-link to="invoiceRecord" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>开票记录</div>
+      </router-link>
+      <router-link to="goodsInfo" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>商品信息</div>
+      </router-link>
+      <router-link to="invoiceInfo" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>开票信息</div>
+      </router-link>
+      <router-link to="test" class="item">
+        <i class="iconfont iconshenfenzheng"></i>
+        <div>test</div>
+      </router-link>
     </div>
 
     <div class="advertising"></div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,57 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import ManualInvoice from '@/components/ManualInvoice/ManualInvoice'
-import ScanInvoice from '@/components/ScanInvoice/ScanInvoice'
-import CheckInvoice from '@/components/CheckInvoice/CheckInvoice'
-import InvoiceRecord from '@/components/InvoiceRecord/InvoiceRecord'
-import GoodsInfo from '@/components/GoodsInfo/GoodsInfo'
-import InvoiceInfo from '@/components/InvoiceInfo/InvoiceInfo'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import ManualInvoice from "@/components/ManualInvoice/ManualInvoice";
+import ScanInvoice from "@/components/ScanInvoice/ScanInvoice";
+import CheckInvoice from "@/components/CheckInvoice/CheckInvoice";
+import InvoiceRecord from "@/components/InvoiceRecord/InvoiceRecord";
+import GoodsInfo from "@/components/GoodsInfo/GoodsInfo";
+import InvoiceInfo from "@/components/InvoiceInfo/InvoiceInfo";
+import Test from "@/components/Test/Test";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home
     },
     {
-      path: '/manualInvoice',
-      name: 'ManualInvoice',
+      path: "/manualInvoice",
+      name: "ManualInvoice",
       component: ManualInvoice
     },
     {
-      path: '/scanInvoice',
-      name: 'ScanInvoice',
+      path: "/scanInvoice",
+      name: "ScanInvoice",
       component: ScanInvoice
     },
     {
-      path: '/checkInvoice',
-      name: 'CheckInvoice',
+      path: "/checkInvoice",
+      name: "CheckInvoice",
       component: CheckInvoice
     },
     {
-      path: '/invoiceRecord',
-      name: 'InvoiceRecord',
+      path: "/invoiceRecord",
+      name: "InvoiceRecord",
       component: InvoiceRecord
     },
     {
-      path: '/goodsInfo',
-      name: 'GoodsInfo',
+      path: "/goodsInfo",
+      name: "GoodsInfo",
       component: GoodsInfo
     },
     {
-      path: '/invoiceInfo',
-      name: 'InvoiceInfo',
+      path: "/invoiceInfo",
+      name: "InvoiceInfo",
       component: InvoiceInfo
     },
+    {
+      path: "/test",
+      name: "Test",
+      component: Test
+    }
   ]
-})
+});
